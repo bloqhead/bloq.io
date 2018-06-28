12882823153e685df785de0d7d38176e097ad588
Daryn St. Pierre <daryn@bigseadesign.com>
2018-06-27 22:31:25 -0400

    Config formatting.

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,12 +2,24 @@ module.exports = {
   head: {
     titleTemplate: '%s | Daryn St. Pierre - Front-End Web Developer',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, inital-scale=1' },
-      { hid: 'description', name: 'description', content: 'Meta description' }
+      {
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, inital-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Meta description'
+      }
     ],
     link: [
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans|Overpass+Mono:400,700' }
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Open+Sans|Overpass+Mono:400,700'
+      }
     ]
   },
   build: {
