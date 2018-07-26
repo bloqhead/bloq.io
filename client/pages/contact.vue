0305bfd137dd3b050f13b0ca02490b918d8b9be5
Daryn St. Pierre <daryn@bigseadesign.com>
2018-07-26 09:28:18 -0400

    Updated the contact page and added a deploy script to the package.json.

--- client/pages/contact.vue
+++ client/pages/contact.vue
@@ -3,7 +3,7 @@
     <h1 class="brush title">{{pageTitle}}</h1>
 
     <div class="intro">
-      <p>If you want to drop me a line to ask a question or just say hi, you can email me at daryniscool [at] gmail.com. I'll try to respond in a day or two.</p>
+      <p>If you want to drop me a line to ask a question or just say hi, you can email me at <a href="mailto:daryniscool@gmail.com">daryniscool@gmail.com</a>. I'll try to respond in a day or two.</p>
     </div>
 
     <!-- <form @submit.prevent="handleSubmit" novalidate class="flex-form">
